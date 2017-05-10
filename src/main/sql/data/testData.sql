dcb206f1b8f67a367167b1a7f55979211b2eeca1
mduby <mduby@broadinstitute.org>
2017-05-10 15:46:31 -0400

    working JPA list of nodes in the db

--- src/main/sql/data/testData.sql
+++ src/main/sql/data/testData.sql
@@ -1,11 +1,11 @@
 
 -- insert 3 sample rows
-insert into registry_server_node (server_node_id, web_name, url, type, last_updated, version)
-  values(1, 'GA4GH beacon', 'http://beacon.ga4gh.org', 'beacon', sysdate(), 0);
+insert into registry_server_node (web_name, url, type, version)
+  values('GA4GH beacon', 'http://beacon.ga4gh.org', 'beacon', 0);
 
-insert into registry_server_node (server_node_id, web_name, url, type, last_updated, version)
-  values(2, 'GA4GH Matchmaker', 'http://matchmaker.ga4gh.org', 'matchmaker', sysdate(), 0);
+insert into registry_server_node (web_name, url, type, version)
+  values('GA4GH Matchmaker', 'http://matchmaker.ga4gh.org', 'matchmaker', 0);
 
-insert into registry_server_node (server_node_id, web_name, url, type, last_updated, version)
-  values(3, 'Brroad Institute beacon', 'http://beacon.broadinstitute.org', 'beacon', sysdate(), 0);
+insert into registry_server_node (web_name, url, type, version)
+  values('Broad Institute beacon', 'http://beacon.broadinstitute.org', 'beacon', 0);
 
